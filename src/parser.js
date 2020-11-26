dbad39766b6cf97bce162772068592243f80febe
Johan Andersson <ja222um@student.lnu.se>
2020-11-26 16:59:37 +0100

    Working on parsing out the instances. Now getting server types.

--- src/parser.js
+++ src/parser.js
@@ -3,16 +3,18 @@ const os = require('os')
 const path = require('path')
 let configFile
 let tfstateFile
+let parsedtfstate
+let serverTypes
 
 const setFile = file => {
   configFile = require('./' + file)
-  console.log(configFile.tfstatePath)
   tfstateFile = fs.readFileSync(getPath(configFile.tfstatePath)).toString()
-  console.log(tfstateFile)
+  parsedtfstate = JSON.parse(tfstateFile)
+  serverTypes = getServerTypes()
 }
 
-const parseServers = servers => {
-
+const parseServers = () => {
+  getInstances()
 }
 
 const parseRKE = () => {
@@ -25,6 +27,18 @@ const parseSSH = () => {
 
 const parseAnsible = () => {
 
+}
+
+const getServerTypes = () => {
+  return parsedtfstate.resources.filter(element => element.type === 'openstack_compute_instance_v2')
+}
+
+const getInstances = () => {
+  const parsedHosts = []
+  let bastionHost = {}
+  serverTypes.forEach(serverType => {
+    console.log(serverType)
+  })
 }
 
 const getPath = tildyPath => {
