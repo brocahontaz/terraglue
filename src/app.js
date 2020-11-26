1490c2d26bae9b3a98e2ff248254ce2269b8a5d5
Johan Andersson <ja222um@student.lnu.se>
2020-11-26 14:44:52 +0100

    Adding option flags.

--- src/app.js
+++ src/app.js
@@ -1,65 +1,30 @@
-import { createRequire } from 'module'
-import * as path from 'path'
-
-import Parser from './parser.js'
-import Creator from './creator.js'
-
-const require = createRequire(import.meta.url)
+const path = require('path')
+const yargs = require('yargs')
 const os = require('os')
 
-try {
-  let config
-  var args = process.argv.slice(2)
-  if (args[0] && typeof args[0] === 'string' && args[0] !== '') {
-    config = require('./configs/' + args[0])
-  } else {
-    config = require('./configs/default.json')
-  }
+const Parser = require('./parser')
+const Creator = require('./creator')
 
-  _setPaths(config)
+const argv = yargs
+  .option('rke', {
+    alias: 'r',
+    description: 'Create config file for rke',
+    type: 'boolean' 
+  })
+  .option('ansible', {
+    alias: 'a',
+    description: 'Create config files for Ansible',
+    type: 'boolean'
+  })
+  .option('ssh', {
+    alias: 's',
+    description: 'Create SSH host file',
+    type: 'boolean'
+  })
+  .help()
+  .alias('help', h)
+  .argv
 
-  const parser = new Parser(config.tfstatePath)
-
-  const creator = new Creator(parser.getNodeInfo(config.nodeTypes), config.ssh, config.ansible, config.rke)
-  creator.createAnsibleHosts()
-  creator.createSSHConfig()
-  creator.createRKEClusterYaml()
-} catch (err) {
-  console.log(err)
-}
-
-/**
- * Set paths correctly.
- *
- * @param {object} config the config.
- */
-function _setPaths (config) {
-  config.tfstatePath = _getPath(config.tfstatePath)
-  config.ssh.configPath = _getPath(config.ssh.configPath)
-  config.ansible.hostsPath = _getPath(config.ansible.hostsPath)
-  config.rke.configPath = _getPath(config.rke.configPath)
-}
-
-/**
- * Helper method to get clean path.
- *
- * @author Johan Andersson
- * @param {string} inputPath the path.
- * @returns {string} clean path.
- */
-function _getPath (inputPath) {
-  const splitPath = inputPath.split(path.sep)
-  _untildifyPath(splitPath)
-  return path.join.apply(null, splitPath)
-}
-
-/**
- * Untildifies a split path.
- *
- * @param {Array} splitPath the path to untildify.
- */
-function _untildifyPath (splitPath) {
-  if (splitPath[0] === '~') {
-    splitPath[0] = os.homedir()
-  }
+if (argv.rke) {
+  console.log('RKE!')
 }
