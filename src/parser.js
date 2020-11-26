dbaf5f89382d0050cf712aa16ac01f3e705b9001
Johan Andersson <ja222um@student.lnu.se>
2020-11-26 17:20:57 +0100

    Now filtering instance IPs by type.

--- src/parser.js
+++ src/parser.js
@@ -5,12 +5,14 @@ let configFile
 let tfstateFile
 let parsedtfstate
 let serverTypes
+let floatingIPAssociations
 
 const setFile = file => {
   configFile = require('./' + file)
   tfstateFile = fs.readFileSync(getPath(configFile.tfstatePath)).toString()
   parsedtfstate = JSON.parse(tfstateFile)
   serverTypes = getServerTypes()
+  floatingIPAssociations = getFloatingIPAssociations()
 }
 
 const parseServers = () => {
@@ -33,17 +35,29 @@ const getServerTypes = () => {
   return parsedtfstate.resources.filter(element => element.type === 'openstack_compute_instance_v2')
 }
 
+const getFloatingIPAssociations = () => {
+  return parsedtfstate.resources.filter(element => element.type === 'openstack_compute_floatingip_associate_v2')
+}
+
 const getAllInstances = () => {
   const parsedHosts = []
   let bastionHost = {}
   serverTypes.forEach(serverType => {
     const instances = getInstancesByType(serverType.name)
-    let instanceIPs = []
+    let instanceIPs = getInstanceIPsByType(serverType.name)
+    console.log(instanceIPs)
   })
 }
 
 const getInstancesByType = type => {
   return serverTypes.filter(instance => instance.name === type)[0].instances
+}
+
+const getInstanceIPsByType = type => {
+
+  const filteredInstances = floatingIPAssociations.filter(instance => instance.name === type)[0]
+
+  return filteredInstances?.instances || []
 }
 
 const getPath = tildyPath => {
